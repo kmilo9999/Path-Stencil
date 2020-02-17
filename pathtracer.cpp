dcb161fdf2e13ec7f751e505787fea275551f1bc
Kmilo9999 <k.diaz99@gmail.com>
2020-02-17 01:11:01 -0500

    emitting light finally working

--- pathtracer.cpp
+++ pathtracer.cpp
@@ -21,7 +21,15 @@ void PathTracer::traceScene(QRgb *imageData, const Scene& scene)
         //#pragma omp parallel for
         for(int x = 0; x < m_width; ++x) {
             int offset = x + (y * m_width);
-            intensityValues[offset] = tracePixel(x, y, scene, invViewMat);
+           // intensityValues[offset] = tracePixel(x, y, scene, invViewMat);
+
+            intensityValues[offset] = Vector3f(0.0,0.0,0.0);
+            for (int rays=0; rays<50; ++rays)
+            {
+                intensityValues[offset] += tracePixel(x, y, scene, invViewMat);
+            }
+            intensityValues[offset] /= 50;
+
         }
     }
 
@@ -31,28 +39,63 @@ void PathTracer::traceScene(QRgb *imageData, const Scene& scene)
 Vector3f PathTracer::tracePixel(int x, int y, const Scene& scene, const Matrix4f &invViewMatrix)
 {
     Vector3f p(0, 0, 0);
+    // view port space
     Vector3f d((2.f * x / m_width) - 1, 1 - (2.f * y / m_height), -1);
     d.normalize();
 
+    //d is direction
     Ray r(p, d);
+    // world space
     r = r.transform(invViewMatrix);
+
+    //std::cout << "HIT     2" << std::endl;
     return traceRay(r, scene);
+
 }
 
 Vector3f PathTracer::traceRay(const Ray& r, const Scene& scene)
 {
-    IntersectionInfo i;
+    IntersectionInfo interesction;
     Ray ray(r);
-    if(scene.getIntersection(ray, &i)) {
-          //** Example code for accessing materials provided by a .mtl file **
-        const Triangle *t = static_cast<const Triangle *>(i.data);//Get the triangle in the mesh that was intersected
+    if(scene.getIntersection(ray, &interesction)) {
+
+      //  std::cout << "HIT---------------" << std::endl;
+        //** Example code for accessing materials provided by a .mtl file **
+        const Triangle *t = static_cast<const Triangle *>(interesction.data);//Get the triangle in the mesh that was intersected
         const tinyobj::material_t& mat = t->getMaterial();//Get the material of the triangle from the mesh
+        const std::vector<CS123SceneLightData> sceneLights = const_cast<Scene&>(scene).getLights();
+        CS123SceneColor lightIntensity;
+        CS123SceneColor directLight ;
+        const Vector3f hitNormal =  t->getNormal(interesction);
+       // std::cout << "HIT***********" << std::endl;
+        for(size_t i = 0 ; i < sceneLights.size();i++)
+        {
+          Vector3f hitPoint = interesction.hit;
+          Vector3f lightPosition(sceneLights[i].pos[0],sceneLights[i].pos[1],sceneLights[i].pos[2]);
+          Vector3f lightDir = hitPoint -lightPosition ;
+          Vector4f lightColor(sceneLights[i].color.x(),sceneLights[i].color.y(),sceneLights[i].color.z(),1);
+          float r2 = lightDir.norm();
+          float distance = sqrtf(r2);
+          lightDir /= distance;
+          //std::cout << "HIT???????????" << std::endl;
+          //lightIntensity =sceneLights[i].color * intensity / (4 * M_PI * r2);
+          //lightIntensity = sceneLights[i].color * 100.0 / (4 * M_PI * r2);
+         // std::cout << "HIT&&&&&&&&&&&" << std::endl;
+          directLight = (lightColor * 10.0 / (4 * M_PI * r2) ) * std::max(0.f, hitNormal.dot(-lightDir));
+
+        }
+
+      //  std::cout << "HIT%%%%%%%%%%%" << std::endl;
         const tinyobj::real_t *d = mat.diffuse;//Diffuse color as array of floats
        // std::cout << d[0] << " " << d[1] << " " << d[2] << std::endl;
 //        const std::string diffuseTex = mat.diffuse_texname;//Diffuse texture name
-        return Vector3f(d[0], d[1], d[2]);
-       // return Vector3f(0, 0, 0);
+       // std::cout << sizeof(d) / sizeof(float) << std::endl;
+        //CS123SceneColor color(d[0], d[1], d[2],1 );
+        //return Vector3f(d[0] ,d[1], d[2]);
+    //     std::cout << "HIT++++++++++++++" << std::endl;
+        return Vector3f(directLight.x() * d[0] , directLight.y() *d[1], directLight.z() *d[2]);
     } else {
+       //   std::cout << "NO HIT" << std::endl;
         return Vector3f(0, 0, 0);
     }
 }
@@ -62,9 +105,11 @@ void PathTracer::toneMap(QRgb *imageData, std::vector<Vector3f> &intensityValues
         for(int x = 0; x < m_width; ++x) {
             int offset = x + (y * m_width);
             Vector3f currentIntensity =intensityValues[offset];
-            imageData[offset] = currentIntensity.norm() > 0 ? qRgb(255.0, 255.0, 255.0)
-                                                            : qRgb(255.0*currentIntensity[0]
-                                                              , 255.0*currentIntensity[1],255.0*currentIntensity[2]);
+            /*imageData[offset] = currentIntensity.norm() > 0 ? qRgb(255.0, 255.0, 255.0)
+                                                            : qRgb(255.0, 40.0, 40.0 );*/
+            imageData[offset] = qRgb(currentIntensity[0] *255.0,
+                                     currentIntensity[1]*255.0,
+                                     currentIntensity[2]*255.0);
         }
     }
 
